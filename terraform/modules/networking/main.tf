87a8871de621367296a139e2f75627061d1a7ceb
yanggautier <yangguole@outlook.com>
2025-06-29 11:17:17 +0200

    add private connection config in firewall

--- terraform/modules/networking/main.tf
+++ terraform/modules/networking/main.tf
@@ -38,7 +38,8 @@ resource "google_compute_firewall" "allow_datastream_to_sql" {
     var.subnetwork_address, # Datastream subnet
     "10.3.0.0/24",        # Private connection subnet for Datastream
     "169.254.0.0/16",     # Google internal networking
-    "${google_compute_global_address.private_ip_alloc.address}/${google_compute_global_address.private_ip_alloc.prefix_length}"
+    "${google_compute_global_address.private_ip_alloc.address}/${google_compute_global_address.private_ip_alloc.prefix_length}",
+    google_datastream_private_connection.private_connection.vpc_peering_config.0.subnet
   ]
   priority    = 500
 }
