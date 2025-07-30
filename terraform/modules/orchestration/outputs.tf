8fa8c157347b73aae2c2fbd82f7409af7e07c05a
yanggautier <yangguole@outlook.com>
2025-07-30 13:50:15 +0200

    delete composer url

--- terraform/modules/orchestration/outputs.tf
+++ terraform/modules/orchestration/outputs.tf
@@ -2,8 +2,3 @@ output "composer_environment_name" {
   description = "Name of the Cloud Composer environment."
   value       = google_composer_environment.dbt_orchestration.name
 }
-
-output "dbt_runner_service_url" {
-  description = "URL of the Cloud Run DBT runner service."
-  value       = google_cloud_run_service.dbt_runner.status[0].url
-}
