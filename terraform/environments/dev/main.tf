3fdccd5f13adeb61d3fa7e214066b2da29c6d3a3
yanggautier <yangguole@outlook.com>
2025-08-08 23:16:33 +0200

    change dag file gcs name

--- terraform/environments/dev/main.tf
+++ terraform/environments/dev/main.tf
@@ -212,7 +212,7 @@ resource "google_container_cluster" "dbt_cluster" {
 data "google_client_config" "default" {}
 
 provider "kubernetes" {
-  host                   = google_container_cluster.dbt_cluster.endpoint
+  host                   = "https://${google_container_cluster.dbt_cluster.endpoint}"
   token                  = data.google_client_config.default.access_token
   cluster_ca_certificate = base64decode(google_container_cluster.dbt_cluster.master_auth[0].cluster_ca_certificate)
   # Explicitly depend on the GKE cluster to ensure it's ready before configuring the Kubernetes provider
