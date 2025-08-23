a2a35ffcf7146602c109aac72b1d063304e8ddc7
yanggautier <yangguole@outlook.com>
2025-08-23 16:43:54 +0200

    add all

---
+++ terraform/modules/dataviz/variables.tf
@@ -0,0 +1,103 @@
+# ---------------------------------- Project level variables ------------------------------
+variable "project_id" {
+  description = "Project ID"
+  type        = string
+}
+
+variable "region" {
+  description = "GCP region for resources"
+  type        = string
+}
+
+variable "environment" {
+  description = "Environment for the resources (e.g., dev, staging, prod)"
+  type        = string
+}
+
+# ---------------------- Superset configuration variables ------------------------------
+variable "superset_namespace" {
+  description = "Superset namespace for GKE"
+  type        = string
+  default     = "superset"
+}
+
+variable "kubernetes_service_account_email" {
+  description = "Email of Kubernetes service account"
+  type        = string
+}
+
+variable "superset_request_cpu" {
+  description = "Superset container requests cpu in Kubernetes"
+  type = string
+  default = "500m"
+}
+
+variable "superset_request_memory" {
+  description = "Superset container requests memory in Kubernetes"
+  type = string
+  default = "1Gi"
+}
+
+variable "superset_limit_cpu" {
+  description = "Superset container limits cpu in Kubernetes"
+  type = string
+  default = "1000m"
+}
+
+variable "superset_limit_memory" {
+  description = "Superset container limits memory in Kubernetes"
+  type = string
+  default = "2Gi"
+}
+
+# ---------------------------------- Proxy configuration variables ------------------------------
+variable "proxy_request_cpu" {
+  description = "Cloud sql proxy container request cpu in Kubernetes"
+  type = string
+  default = "100m"
+}
+
+variable "proxy_request_memory" {
+  description = "Cloud sql proxy container request memory in Kubernetes"
+  type = string
+  default = "128Mi"
+}
+
+variable "proxy_limit_cpu" {
+  description = "Cloud sql proxy container request cpu in Kubernetes"
+  type = string
+  default = "200m"
+}
+
+variable "proxy_limit_memory" {
+  description = "Cloud sql proxy container request cpu in Kubernetes"
+  type = string
+  default = "256Mi"
+}
+
+# ------------------------- Cloud SQL variables---------------------------------- 
+variable "cloud_sql_instance_name" {
+  description = "Name of PostgreSQL Cloud SQL instance"
+  type        = string
+}
+
+variable "superset_database_user_name"{
+  description = "Cloud SQL PostgreSQL database user name for Superset"
+  type = string
+}
+
+variable "superset_db_password"{
+  description = "Cloud SQL PostgreSQL database password for Superset"
+  type = string
+}
+
+variable "superset_database_name"{
+  description = "Cloud SQL PostgreSQL database name for Superset"
+  type = string
+}
+
+# --------------------------- Memorystore variables -----------------------------------
+variable "superset_redis_cache_host" {
+  description = "Host of Redis Memorystore"
+  type = string
+}
